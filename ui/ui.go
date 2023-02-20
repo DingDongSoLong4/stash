604b25253bd2c49537a477f2e905c8402a4db6c7
DingDongSoLong4 <99329275+DingDongSoLong4@users.noreply.github.com>
2023-02-20 14:53:58 +0200

    Add UI brotli compression

--- ui/ui.go
+++ ui/ui.go
@@ -4,15 +4,19 @@ import (
 	"embed"
 	"io/fs"
 	"runtime"
+
+	"github.com/vearutop/statigz"
 )
 
 //go:embed v2.5/build
 var uiBox embed.FS
 var UIBox fs.FS
+var UIServer *statigz.Server
 
 //go:embed login
 var loginUIBox embed.FS
 var LoginUIBox fs.FS
+var LoginUIServer *statigz.Server
 
 func init() {
 	var err error
@@ -20,11 +24,13 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+	UIServer = statigz.FileServer(UIBox.(fs.ReadDirFS))
 
 	LoginUIBox, err = fs.Sub(loginUIBox, "login")
 	if err != nil {
 		panic(err)
 	}
+	LoginUIServer = statigz.FileServer(LoginUIBox.(fs.ReadDirFS))
 }
 
 type faviconProvider struct{}
