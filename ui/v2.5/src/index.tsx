831a41efc12cca7cb6be7a8f96423a59743abf0d
DingDongSoLong4 <99329275+DingDongSoLong4@users.noreply.github.com>
2023-11-30 15:24:39 +0200

    Remove serviceWorker.ts

--- ui/v2.5/src/index.tsx
+++ ui/v2.5/src/index.tsx
@@ -5,7 +5,6 @@ import { App } from "./App";
 import { getClient } from "./core/StashService";
 import { baseURL, getPlatformURL } from "./core/createClient";
 import "./index.scss";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <>
@@ -26,8 +25,3 @@ ReactDOM.render(
 const script = document.createElement("script");
 script.src = getPlatformURL("javascript").toString();
 document.body.appendChild(script);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
